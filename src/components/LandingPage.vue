a6df13eacbabdd0f0e9be4c41753711578dcb830
Thato Mokoena <thato.mokoena@alumniserv.com>
2025-01-29 08:59:50 +0200

    add check when creating link tracking record

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,27 +3,55 @@ import '@/assets/main.css';
 import { onMounted } from 'vue';
 import type { Schema } from '../../amplify/data/resource';
 import { generateClient } from 'aws-amplify/data';
+import { off } from 'node:process';
 
 const client = generateClient<Schema>();
 
-function getUrlParams() {
+async function isUniqueLink(product_id: string, base_number: string, offer_id: string) {
+  const {data: links, errors} = await client.models.LinkTracking.list({
+    filter: {
+      productId: {
+        eq: product_id
+      },
+      baseNumber: {
+        eq: base_number
+      },
+      offerId: {
+        eq: offer_id
+      }
+    }
+  });
+  console.log(errors);
+  // Return true if no existing record was found (unique), false otherwise
+  return links.length === 0;
+}
+
+async function getUrlParams() {
   console.log("getUrlParams");
   let urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.has('product_id') && urlParams.has('base_number') && urlParams.has('offer_id')){
-    let product_id = urlParams.get('product_id');
-    let base_number = urlParams.get('base_number');
-    let offer_id = urlParams.get('offer_id');
-    
+
+  const product_id = urlParams.get('product_id');
+  const base_number = urlParams.get('base_number');
+  const offer_id = urlParams.get('offer_id');
+
+  if (product_id && base_number && offer_id){
     console.log(product_id);
     console.log(base_number);
     console.log(offer_id);
 
-    // if(client.models.LinkTracking.get())
-    client.models.LinkTracking.create({
-      productId: product_id,
-      baseNumber: base_number,
-      offerId: offer_id
-    });   
+    const isUnique = await isUniqueLink(product_id, base_number, offer_id);
+
+    if(isUnique){
+      await client.models.LinkTracking.create({
+        productId: product_id,
+        baseNumber: base_number,
+        offerId: offer_id
+      });
+    } else {
+      console.log("Duplicate LinkTracking record found. Not creating a new one.");
+    }
+  } else {
+    console.error("Missing URL parameters: product_id, base_number, or offer_id.");
   }
 }
     
